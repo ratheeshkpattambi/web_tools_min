fd95a4f80df23e0284a4237af2132d19251d3b99
Jamsheed <91758030+Jamsheed-CS@users.noreply.github.com>
2025-05-22 21:11:20 -0700

    Fix routing and update tests

--- src/common/metadata.js
+++ src/common/metadata.js
@@ -225,7 +225,7 @@ export function generateMetaTags(path) {
     <meta property="og:title" content="${title}">
     <meta property="og:description" content="${description}">
     <meta property="og:type" content="website">
-    <meta property="og:url" content="https://safewebtool.com${path === '/' ? '' : `/${path}`}">
+    <meta property="og:url" content="https://safewebtool.com${path === '/' ? '' : path}">
     <meta name="twitter:card" content="summary">
     <meta name="twitter:title" content="${title}">
     <meta name="twitter:description" content="${description}">
